39a12ff52d0bd9c25aed4bb35ef3236a2c22ab31
JoanneZamorano <joannezamorano@gmail.com>
2022-07-18 15:41:43 +0200

    S8.1callbacks ejercicios hechos y comprendidos

--- S8/1.callbacks/ex2.js
+++ S8/1.callbacks/ex2.js
@@ -1,30 +1,30 @@
+//COMPRENDIDO
 
-/*Usa el siguiente código como base y crea 3 funciones llamadas father, confirmExample, 
-promptExample. La función confirmExample recibirá una variable de tipo string (description) 
-que mostrará como titulo de la ventana y retornará el valor del confirm. La función 
-promptExample hará lo mismo pero con un prompt. La función father recibirá como parámetros 
-description, con el valor del titulo de las ventanas y una función callback (confirmExample 
-o promptExample).
+/*Usa el siguiente código como base y crea 3 funciones llamadas father, confirmExample, promptExample. La función confirmExample recibirá una variable
+de tipo string (description) que mostrará como titulo de la ventana y retornará el valor del confirm. La función promptExample hará lo mismo pero 
+con un prompt. La función father recibirá como parámetros description, con el valor del titulo de las ventanas y una función callback 
+(confirmExample o promptExample).
 
-La función father deberá ejecutar la función que reciba como callback y añadir el valor 
-resultado que retorne la función al array userAnwsers.
+La función father deberá ejecutar la función que reciba como callback y añadir el valor resultado que retorne la función al array userAnwsers.*/
 
-Ejecuta varias veces la función father y haz finalmente un console.log de userAnwsers*/
 
 const userAnwsers = [];
 
-function confirmExample(){
-    confirm()
+function confirmExample(description){  //recibirá una variable de tipo string (description) que mostrará como titulo de la ventana y retornará el valor del confirm.
+    return confirm(description)
 }
 
-function promptExample(){
-    propmt()
+function promptExample(description){  //hará lo mismo pero con un prompt.
+    return propmt(description)
 }
 
-function father(){
-
+function father(description, callback){  //recibirá como parámetros description, con el valor del titulo de las ventanas y una función callback (confirmExample o promptExample).
+    userNames.push(callback(description))
 }
 
-// guia de confirm y prompt
-// const confirmValue = confirm('Soy un texto');
-// const propmt = prompt('Soy un texto');
+/*Ejecuta varias veces la función father y haz finalmente un console.log de userAnwsers*/
+
+father('holaaaaaaaaa soy el ejemplo de confirm', confirmExample);
+father('y yo el ejemplo de proooooomp', promptExample);
+
+console.log(userAnwsers);

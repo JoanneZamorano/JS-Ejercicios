843e4dd707594784b068500b5dd832a552fdba53
JoanneZamorano <joannezamorano@gmail.com>
2022-07-25 12:48:01 +0200

    s7, carpeta filter ex 7 y  8 no realizados, no comprendidos

--- S7/5.filter/ex8.js
+++ S7/5.filter/ex8.js
@@ -1,7 +1,6 @@
 //NO COMPRENDIDO / NO REALIZADO
 
 
-
 //Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola los 
 //streamers que incluyan la palabra introducida en el input. De esta forma, si 
 //introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
